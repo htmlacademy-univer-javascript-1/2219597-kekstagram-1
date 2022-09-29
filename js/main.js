983ef9f0e02499dc92fa2e1bb2c43e5764a58dab
Arina <Arina.Kuptsova@urfu.me>
2022-09-29 23:29:43 +0500

    2.17.Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let getRandomIntInclusive = (min, max) => {
+  if (min <0 || max <0){
+    return 0;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let checkLength = (str, max_length) => {
+  if (str.length <= max_length) {
+    return true;
+  }
+  return false;
+}
