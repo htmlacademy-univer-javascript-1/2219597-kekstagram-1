7751dd9c16e469eba4973fd59112ad954c22bb92
Keks <keks@htmlacademy.ru>
2022-12-05 21:27:43 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,58 @@
+import {getRandomPositiveInteger, getRandomArrayElement, getId, getCommentId} from './util.js';
+
+const NAMES = [
+  'Кирилл',
+  'Софья',
+  'Юлия',
+  'Арина',
+  'Виктор',
+  'Светлана',
+  'Даниил',
+  'Вероника',
+  'Дмитрий',
+  'Алёна',
+];
+
+const USERS_COUNT = 25;
+const COMMENTS_COUNT = 4;
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const DESCRIPTION = [
+  'Это я в горах, загораю',
+  'Кому привезти открытку с отпуска?',
+  'Жизнь прекрасна!',
+  'Сегодня думал о смысле жизни, ничего придумать не удалось.',
+  'Обычно здесь что-то пишут, но я пока не придумал',
+  'Моя любимая фотка',
+];
+
+const getComment = () =>
+  ({
+    id: getCommentId(),
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+    message: getRandomArrayElement(COMMENTS),
+    name: getRandomArrayElement(NAMES),
+  });
+
+let x = 0;
+const createPost = () => {
+  x = getId();
+  return {
+    id: x,
+    url: `photos/{{{${x}}}.jpg`,
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomPositiveInteger(15, 200),
+    comments: Array.from({length: COMMENTS_COUNT}, getComment),
+  };
+};
+
+const createPosts = () => Array.from({length: USERS_COUNT}, createPost);
+export {createPosts};
---
+++ 6/js/util.js
@@ -0,0 +1,25 @@
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const checkStringLength = (string, length) => (string.length <= length);
+
+checkStringLength();
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+let number, numberComment = 0;
+const getId = () => {
+  number ++;
+  return number;
+};
+
+const getCommentId = () => {
+  numberComment ++;
+  return numberComment;
+};
+
+export {getRandomPositiveInteger, getRandomArrayElement, getId, getCommentId};
